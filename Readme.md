21c5d6395969dd4077366740cda7bfc92032047d
Tad DeVries <tad@splunk.net>
2013-09-29 18:43:25 -0600

    inital commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+## lazyboy
+Package to wrap the CouchDB initialization for inclusion into the revel framework.
+
+## Usage
+Include the following lines to app.conf in your revel application to import the module into your application.
+
+### app.conf
+`module.lazyboy = github.com/taddevries/lazyboy`
+
+
---
+++ lazyboy.go
@@ -0,0 +1,39 @@
+package lazyboy
+
+import (
+	"github.com/robfig/revel"
+	"errors"
+)
+
+var (
+	Url	string
+	Port	string
+	Username	string
+	Password	string
+	Name	string
+)
+
+func AppInit() {
+		var found bool
+		if Url, found = revel.Config.String("couchdb.url"); !found {
+			err := errors.New("lazyboy: Database URL not defined in app.conf")
+			revel.ERROR.Panic(err)
+		}
+
+		if Port, found = revel.Config.String("couchdb.port"); !found {
+			Port = "5984"
+		}
+
+		if Username, found = revel.Config.String("couchdb.username"); !found {
+			Username = ""
+		}
+
+		if Password, found = revel.Config.String("couchdb.password"); !found {
+			Password = ""
+		}
+
+		if Name, found = revel.Config.String("couchdb.name"); !found {
+			err := errors.New("lazyboy: Database name not defined in app.conf")
+			revel.ERROR.Panic(err)
+		}
+}
