7528dd45daa9d41d9754e46732c4fd9163c1f246
Tad DeVries <tad@splunk.net>
2013-09-29 18:48:46 -0600

    go fmt

--- lazyboy.go
+++ lazyboy.go
@@ -1,39 +1,39 @@
 package lazyboy
 
 import (
-	"github.com/robfig/revel"
 	"errors"
+	"github.com/robfig/revel"
 )
 
 var (
-	Url	string
-	Port	string
-	Username	string
-	Password	string
-	Name	string
+	Url      string
+	Port     string
+	Username string
+	Password string
+	Name     string
 )
 
 func AppInit() {
-		var found bool
-		if Url, found = revel.Config.String("couchdb.url"); !found {
-			err := errors.New("lazyboy: Database URL not defined in app.conf")
-			revel.ERROR.Panic(err)
-		}
+	var found bool
+	if Url, found = revel.Config.String("couchdb.url"); !found {
+		err := errors.New("lazyboy: Database URL not defined in app.conf")
+		revel.ERROR.Panic(err)
+	}
 
-		if Port, found = revel.Config.String("couchdb.port"); !found {
-			Port = "5984"
-		}
+	if Port, found = revel.Config.String("couchdb.port"); !found {
+		Port = "5984"
+	}
 
-		if Username, found = revel.Config.String("couchdb.username"); !found {
-			Username = ""
-		}
+	if Username, found = revel.Config.String("couchdb.username"); !found {
+		Username = ""
+	}
 
-		if Password, found = revel.Config.String("couchdb.password"); !found {
-			Password = ""
-		}
+	if Password, found = revel.Config.String("couchdb.password"); !found {
+		Password = ""
+	}
 
-		if Name, found = revel.Config.String("couchdb.name"); !found {
-			err := errors.New("lazyboy: Database name not defined in app.conf")
-			revel.ERROR.Panic(err)
-		}
+	if Name, found = revel.Config.String("couchdb.name"); !found {
+		err := errors.New("lazyboy: Database name not defined in app.conf")
+		revel.ERROR.Panic(err)
+	}
 }
